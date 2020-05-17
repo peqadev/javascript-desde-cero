aa17b18882ab5af9cc7063b556ec993801db1f2b
Ercik Agrazal <erick.agrazal@balboadevelopers.com>
2020-05-16 20:24:19 -0500

    Added generales.

---
+++ 2. conceptos_basicos/2.1_conceptos_basicos.js
@@ -0,0 +1,6 @@
+var Früh = "foobar";
+
+// Case Sensitive
+// Las instrucciones se llaman Sentencias
+// Las instrucciones se separan por punto y coma (Opcional)
+// El texto se escanea de Izquierda a Derecha
---
+++ 2. conceptos_basicos/2.2_comentarios.js
@@ -0,0 +1,7 @@
+// comentario en una sola línea
+
+/* este es un
+   comentario multilínea
+*/
+
+/* no puedes, sin embargo, /* anidar comentarios */ SyntaxError */
